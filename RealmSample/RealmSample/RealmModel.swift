f337f36c9f4db9f08b524b7d988adc7ac10a0011
Apollo <a5347354@gmail.com>
2016-12-19 18:19:44 +0800

    Realm 1.Insert 2.Delete 3.Select

--- RealmSample/RealmSample/RealmModel.swift
+++ RealmSample/RealmSample/RealmModel.swift
@@ -8,16 +8,11 @@
 
 import RealmSwift
 
-class HouseInfoRealm: Object {
+class RealmModel: Object {
     dynamic var id : String?
     dynamic var title: String? //標題
-    dynamic var simpleIntro: String? //簡短介紹
-    dynamic var landlordName : String? //房東姓名
-    dynamic var address : String? //地址
-    dynamic var telephone : String? //電話
-    dynamic var isRent : Int = 0//是否已出租
-    
-    //MARK: 格式為字串
+   
+    //MARK: Error
     //    dynamic var pictures : [String]?//房屋圖片
 
 }
--- RealmSample/RealmSample/ViewController.swift
+++ RealmSample/RealmSample/ViewController.swift
@@ -7,10 +7,15 @@
 //
 
 import UIKit
-
+import RealmSwift
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var inputText: UITextField!
+    @IBOutlet weak var titleText: UITextField!
+    @IBOutlet weak var showDataLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,7 +25,59 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //MARK: 新增資料
+    @IBAction func insertData(_ sender: AnyObject) {
+        let realmModel = RealmModel()
+        let realm = try! Realm()
+        
+        realmModel.id = inputText.text
+        realmModel.title = titleText.text
+        
+        try! realm.write {
+            realm.add(realmModel)
+        }
+        showDataLabel.text = "新增\(realmModel.id)與\(realmModel.title)"
+    }
+    
+    
+    //搜尋資料
+    @IBAction func showData(_ sender: AnyObject) {
+        
+        let realm = try! Realm()
+        
+        //MARK: 搜尋條件資料
+        let predicate = NSPredicate(format: "id = %@ OR title BEGINSWITH %@", inputText.text!, titleText.text!)
+        let datas = realm.objects(RealmModel.self).filter(predicate)
+        
+        
+        //MARK: 全部資料
+        let allDatas = realm.objects(RealmModel.self)
+        
+        var strs: String = ""
+        for (index,element) in allDatas.enumerated() {
+            strs += "[\(index)]\(element.title)"
+        }
+        
+        if strs == "" {
+            strs = "未輸入條件值或找不到"
+        }
+        showDataLabel.text = strs
+    }
+    
 
-
+    //MARK: 刪除資料
+    @IBAction func deleteData(_ sender: AnyObject) {
+        
+        let realm = try! Realm()
+        try! realm.write {
+            //刪除全部
+            realm.deleteAll()
+        }
+        
+        showDataLabel.text = "已刪除"
+    }
+    
+    
 }
 
